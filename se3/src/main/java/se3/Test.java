41cb1a527a825370d2faebb8dd3dc975190f6223
kuguk <kuguk@ukr.net>
2021-03-09 21:42:24 +0200

    modyf. code

--- se3/src/main/java/se3/Test.java
+++ se3/src/main/java/se3/Test.java
@@ -3,13 +3,9 @@ package se3;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
-import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -18,63 +14,50 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class Test {
-//	protected static URL gridHubUrl;
-//	  protected static String baseUrl;
-//	  protected static Capabilities capabilities;
-//
-//	  protected WebDriver driver;
-//	
-//	@BeforeAll
-//	  public static void loadConfig() throws Throwable {
-//	    SuiteConfiguration config = new SuiteConfiguration();
-//	    baseUrl = config.getProperty("site.url");
-//	    if (config.hasProperty("grid.url") && !"".equals(config.getProperty("grid.url"))) {
-//	      gridHubUrl = new URL(config.getProperty("grid.url"));
-//	    }
-//	    capabilities = config.getCapabilities();
-//	  };
-//	
-
+	
 	private static WebDriver driver;
     private static WebDriverWait wait;
     private static String login;
     private static String password;
     private static String site_url;
     private static String pathToChromeDriver;
+    private static String stop;
     
     
-	private static void set_conf() throws IOException {
+    private static void set_conf() throws IOException {
         try(InputStream inputStream = new FileInputStream("./conf.properties")) {
             Properties properties = new Properties();
             properties.load(inputStream);
+            
             login = properties.getProperty("login");
             password = properties.getProperty("password");
             pathToChromeDriver = properties.getProperty("pathToChromeDriver");
+            site_url = properties.getProperty("site.url");
             
             System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
             driver = new ChromeDriver();
-	        wait = new WebDriverWait(driver, 10);	
+
+            wait = new WebDriverWait(driver, 10);
+	        
         } catch (IOException io) {
-            io.printStackTrace();
+        	System.out.println(io.getMessage());
+        }
+         catch (Exception ex) {        	
+        	System.out.println(ex.getMessage());
+        	stop = "err";
         }
 	}
-
 	
-	public static void main(String[] args) throws InterruptedException, IOException {
-	
+  public static void main(String[] args) throws InterruptedException, IOException {
+		
 	set_conf();
-	
-	//System.setProperty("webdriver.chrome.driver", "./lib/chromedriver.exe");	
-	//System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
-    
-	//WebDriver driver = new ChromeDriver();
-	//WebDriverWait wait = new WebDriverWait(driver, 10);	
-	
-	
-	driver.navigate().to("https://github.com");
+
+	if (stop == "err") return;
+		
+	driver.navigate().to(site_url);
 	driver.manage().window().maximize();
 			  		
-// Login to GitHub account
+/* Login to GitHub account */
 	wait.until(ExpectedConditions.elementToBeClickable(By.partialLinkText("Sign in")));  	
 	driver.findElement(By.partialLinkText("Sign in")).click();
 		
@@ -82,37 +65,37 @@ public class Test {
 	driver.findElement(By.name("login")).sendKeys(login);
 	driver.findElement(By.name("password")).sendKeys(password);	
 
-	// Form submit
+    /* Form submit  */
 	driver.findElement(By.name("password")).submit();	
 
-// Create a new repository:
+/* Create a new repository:  */
 	
-	// Call up-right menu
+	/* Call up-right menu */
 	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//summary[@class='Header-link'][*[@class='octicon octicon-plus']]"))); 
 	driver.findElement(By.xpath("//summary[@class='Header-link'][*[@class='octicon octicon-plus']]")).click();
 	driver.findElement(By.partialLinkText("New repository")).click();
 	
-	// Creation of a new repository
+	/* Creation of a new repository  */
 	driver.findElement(By.id("repository_name")).sendKeys("TestRepository");
 	driver.findElement(By.id("repository_gitignore_template_toggle")).click();
 	
-	// add "ignore" file
+	/* Add "ignore" file  */
 	driver.findElement(By.xpath("//i[contains(text(),'.gitignore template:')]")).click();	
 	driver.findElement(By.xpath("//span[contains(text(),'Java')]")).click();
 
-	//Form submit
+	/* Form submit  */
 	driver.findElement(By.id("repository_gitignore_template_toggle")).submit();
 	
-	// Adding a README-file:
+	/* Adding a README-file:  */
 	driver.findElement(By.xpath("//a[contains(text(),'Add a README')]")).click();
       //Change content README-file
 	driver.findElement(By.xpath("//span[@role='presentation']")).sendKeys(Keys.chord(Keys.CONTROL, "a"),"My Test Repository");
 	driver.findElement(By.xpath("//input[@id='commit-summary-input']")).sendKeys("Add readme file");
 
-	// Commit
+	/* Commit  */
 	driver.findElement(By.xpath("//input[@id='commit-summary-input']")).submit();
 	
-// repository Removing
+/* Repository removing  */
 	driver.findElement(By.xpath("//span[@data-content='Settings']")).click();
 	
 	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//summary[contains(text(),'Delete this repository')]")));
@@ -121,18 +104,17 @@ public class Test {
 	driver.findElement(By.xpath("//input[@aria-label='Type in the name of the repository to confirm that you "
 			+ "want to delete this repository.']")).sendKeys("kuguk/TestRepository");
 
-	//Form submit
+	/* Form submit */
 	driver.findElement(By.xpath("//input[@aria-label='Type in the name of the repository to confirm that you "
 			+ "want to delete this repository.']")).submit();
 	
- //Sign out
+/* Sign out  */
 	driver.findElement(By.xpath("//summary[@class='Header-link'][img[@alt='@kuguk']]")).click();
 
 	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'Sign out')]")));
 	driver.findElement(By.xpath("//button[contains(text(),'Sign out')]")).click();
 
 	//Thread.sleep(5000);
- 
 	driver.quit();
 	 
 	}
